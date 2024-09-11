f36453c339d449d42d98cef97e7ccccb16a859e4
Chief <brianmwangi10808@gmail.com>
2024-09-11 09:13:35 +0300

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Adjust the base URL path if needed
   plugins: [react()],
   build: {
-    outDir: 'dist' // This specifies the output directory
+    outDir: 'dist'
   }
 })
